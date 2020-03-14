6c18bf36f92c864cebee004e4b20543bfe9a78d5
mishu <mishu@xtelecom.ro>
2020-03-14 18:23:26 +0200

    removes Types from migration

--- src/database/migrations/2017_12_07_150100_create_structure_for_countries.php
+++ src/database/migrations/2017_12_07_150100_create_structure_for_countries.php
@@ -1,11 +1,10 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForCountries extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.countries.options', 'description' => 'Get country options for select', 'type' => Types::Read, 'is_default' => false],
+        ['name' => 'core.countries.options', 'description' => 'Get country options for select', 'is_default' => false],
     ];
 }
