6eca132425e9305a43b0add093679d1ea453a21f
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 13:33:15 +0200

    updated migrations

--- database/migrations/2017_12_07_150100_create_structure_for_countries.php
+++ database/migrations/2017_12_07_150100_create_structure_for_countries.php
@@ -2,9 +2,9 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForCountries extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'core.countries.options', 'description' => 'Get country options for select', 'is_default' => false],
     ];
-}
+};
